4b281fd7d9db25edf6474cbb381fd24f3819f1f6
Gary11777 <iharushakou@gmail.com>
2024-01-16 19:15:10 +0300

    36th lesson first commit

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,13 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task delete successfully!');
+})->name('task.destroy');
+
 /*Route::get('/xxx', function () {
     return 'Hello';
 })->name('hello');
